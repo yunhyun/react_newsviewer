b3c8fcb4923614bcdba78aef037d1c0c60165bca
yunhyun <cyh0107@gmail.com>
2020-11-13 11:54:02 +0900

    카테고리적용

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-import React, { useState } from 'react';
+// 카테고리 값을 useState 로 관리
+import React, { useState, useCallback } from 'react';
 import axios from 'axios';
 import NewsList from './components/NewsList';
+import Categories from './components/Categories';
 
 const App = () => {
-  return <NewsList />;
+  const [category, setCategory] = useState('all');
+  const onSelect = useCallback(category => setCategory(category), []);
+  return (
+    <>
+    {/* category 작성 후 Categories.js 에서 props 설정 */}
+    <Categories category={category} onSelect={onSelect} />
+    <NewsList category={category} />
+    </>
+  );
 };
 
 export default App;
